1c86f15ff449ce6c34acc6f68c067b1d4c01cf4d
Usuario <Usuario@DESKTOP-QAROUMS>
2023-06-04 20:40:39 -0600

    Coca-Cola

---
+++ src/main/java/com/CocaColaPract/CocaColaPractApplication.java
@@ -0,0 +1,13 @@
+package com.CocaColaPract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocaColaPractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocaColaPractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CocaColaPract/CocaColaPractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CocaColaPract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocaColaPractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
